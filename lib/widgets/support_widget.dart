c918aed409201384f0a9049d7c45dc6ebd34aa5d
dbooker5 <bookerotieno21@gmail.com>
2026-01-20 14:23:16 +0300

    feat: Add support widget for text styles
    Creates a new `support_widget.dart` file.
    This introduces an `AppWidget` class with a static method `boldTextstyle` which returns a bold `TextStyle` with a specified size and the "Roboto" font family.

---
+++ lib/widgets/support_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+  static TextStyle boldTextstyle(double size) {
+    return TextStyle(
+      color: Colors.black,
+      fontSize: size,
+      fontWeight: FontWeight.bold,
+      fontFamily: "Roboto",
+    );
+  }
+}
