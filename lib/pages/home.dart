e6cd8a518d6909f22de04e683b15d518401b844d
dbooker5 <bookerotieno21@gmail.com>
2026-01-22 14:32:26 +0300

    feat: Enhance search bar and add category section
    Refactors the search `TextField` by adding padding, a hint, rounded borders, and a separate search icon button.
    
    Also introduces a static banner image and a new "Category" section. The category section currently includes two "T-Shirts" category icons with labels, arranged in a `Row`.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -35,13 +35,95 @@ class _HomeState extends State<Home> {
             ),
             SizedBox(height: 20.0),
 
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.only(left: 20.0),
+                    margin: EdgeInsets.only(right: 20.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: const Color.fromARGB(86, 0, 0, 0),
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    width: MediaQuery.of(context).size.width,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search for Clothing's..",
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(12.0),
+                  margin: EdgeInsets.only(right: 20.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xff6e5038),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Icon(Icons.search, color: Colors.white),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+
             Container(
               margin: EdgeInsets.only(right: 20.0),
-              decoration: BoxDecoration(border: Border.all()),
-              width: MediaQuery.of(context).size.width,
-              child: TextField(
-                decoration: InputDecoration(border: InputBorder.none),
-              ),
+              child: Image.asset("images/banner.png"),
+            ),
+            SizedBox(height: 10.0),
+
+            Text("Category", style: AppWidget.boldTextstyle(24.0)),
+            SizedBox(height: 10.0),
+
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 235, 224, 211),
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                      child: Image.asset(
+                        "images/t-shirt.png",
+                        height: 40,
+                        width: 40,
+                        fit: BoxFit.cover,
+                        color: Color(0xff6e5038),
+                      ),
+                    ),
+                    SizedBox(height: 5.0),
+                    Text("T-Shirts", style: AppWidget.boldTextstyle(18.0)),
+                  ],
+                ),
+                SizedBox(width: 20.0),
+
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(15.0),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 235, 224, 211),
+                        borderRadius: BorderRadius.circular(60),
+                      ),
+                      child: Image.asset(
+                        "images/t-shirt.png",
+                        height: 40,
+                        width: 40,
+                        fit: BoxFit.cover,
+                        color: Color(0xff6e5038),
+                      ),
+                    ),
+                    SizedBox(height: 5.0),
+                    Text("T-Shirts", style: AppWidget.boldTextstyle(18.0)),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
