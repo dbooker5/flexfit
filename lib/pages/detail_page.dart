9f2cd5fd1a1dd6b569cbc5879691687b3baf2d6b
dbooker5 <bookerotieno21@gmail.com>
2026-01-26 18:38:23 +0300

    feat: Create initial Detail page
    Adds a new `DetailPage` widget.
    The page displays a full-height image (`images/.men-shirt.png`) within a `Scaffold`.

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({super.key});
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Image.asset(
+              "images/.men-shirt.png",
+              height: MediaQuery.of(context).size.height,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
