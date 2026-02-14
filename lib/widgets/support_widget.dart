55cb5b01bfcbdfeaf049826ac8faf51105141c51
dbooker5 <bookerotieno21@gmail.com>
2026-02-14 13:57:35 +0300

    feat: Add selected and non-selected size widgets
    Adds two new static methods to the `SupportWidget` class:
    
    - `nonSelected`: A widget for displaying an unselected size with a border.
    - `selectedone`: A widget for displaying a selected size with a background color.

--- lib/widgets/support_widget.dart
+++ lib/widgets/support_widget.dart
@@ -18,4 +18,28 @@ class AppWidget {
       fontFamily: "Roboto",
     );
   }
+
+  static Widget nonSelected(String size) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+        border: Border.all(color: Color.fromARGB(88, 0, 0, 0)),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Center(child: Text(size, style: AppWidget.boldTextstyle(18))),
+    );
+  }
+
+  static Widget selectedone(String size) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+        color: Color(0xff6e5038),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: Center(child: Text(size, style: AppWidget.whiteTextstyle(18.0))),
+    );
+  }
 }
