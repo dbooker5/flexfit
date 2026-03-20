7d0062fb54f77c26ce37c73bb6a2198e8d4c74d2
dbooker5 <bookerotieno21@gmail.com>
2026-03-20 19:37:39 +0300

    feat: Add product-related methods to DatabaseMethods
    Expands the `DatabaseMethods` class to include Firestore operations for products.
    
    Key changes include:
    - Added `addProducts` to save new product information to the "Products" collection.
    - Added `getAllProducts` to stream all documents from the "Products" collection.
    - Added `getProductsByCategory` to stream products filtered by a specific category.
    - Organized the class with comments to distinguish between user and product methods.

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//User Methods
+
 class DatabaseMethods {
   Future addUserInfo(Map<String, dynamic> userInfoMap, String id) async {
     return await FirebaseFirestore.instance
@@ -10,5 +12,26 @@ class DatabaseMethods {
 
   Future<DocumentSnapshot> getUserDetails(String id) async {
     return await FirebaseFirestore.instance.collection("Users").doc(id).get();
+  }
+
+  // Product Methods
+
+  Future addProducts(Map<String, dynamic> productInfoMap) async {
+    return await FirebaseFirestore.instance
+        .collection("Products")
+        .add(productInfoMap);
+  }
+
+  // Method to fetch all products for your Home Page
+  Stream<QuerySnapshot> getAllProducts() {
+    return FirebaseFirestore.instance.collection("Products").snapshots();
+  }
+
+  // Method to fetch products by category for your Category Page
+  Stream<QuerySnapshot> getProductsByCategory(String category) {
+    return FirebaseFirestore.instance
+        .collection("Products")
+        .where("Category", isEqualTo: category)
+        .snapshots();
   }
 }
